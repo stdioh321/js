2702592d3e003aa63f1598f14ba775c7f5e8b7c0
John Roberto <diaslino@gmail.com>
2016-05-26 15:35:30 -0300

    newest

--- meteor/sample/client/main.js
+++ meteor/sample/client/main.js
@@ -2,9 +2,9 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Mongo } from 'meteor/mongo';
-
+import { Session } from 'meteor/session';
 // import 'bootstrap/dist/css/bootstrap.css';
-import 'importTest.js';
+
 import './main.html';
 
 
@@ -27,7 +27,24 @@ Template.body.helpers({
             return [{id:1, name:"Carlos"}];
         }
     ,"testDB": function(){
-            return DB.find();
+        sort = {sort:{name : 0}}
+        if(Session.get("hideCheck")){
+            return DB.find({checked:{$ne:true}}, sort);
+        }else{
+            return DB.find({},sort);
+        }
+    },
+    hideCheck(){
+        return Session.get("hideCheck");
+    },
+    testDB2(){
+        return DB.find({checked:{$ne:true}});
+        // if(Session.get("hideCheck")){
+        // }else{
+        //     return DB.find()
+        // }
+    },tmpVal(){
+        return Session.get("hideCheck");
     }
 });
     
@@ -38,17 +55,31 @@ Template.body.events({
             name: ev.target.name.value,
             email: ev.target.email.value
         });
-            ev.target.name.value = "";
-            ev.target.email.value = "";
-            return false;
+        ev.target.name.value = "";
+        ev.target.email.value = "";
+        return false;
     },
-    "click .tb-data a": function(ev){
+    "click .tb-data .remove": function(ev){
         DB.remove(this._id);
         return false;
     },
     "change .item-check" : function(ev){
+        console.log(ev);
         DB.update(this._id, {$set: {checked: !this.checked}});
+    },
+    "change .hideCheck": function(ev){
+        console.log(ev);
+        tmp = false
+        if (ev.target.checked) {
+            tmp = true;
+        }
+
+        Session.set("hideCheck", tmp);
     }
+});
+
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY'
 });
 //Template.hello.helpers({
 //  counter() {
